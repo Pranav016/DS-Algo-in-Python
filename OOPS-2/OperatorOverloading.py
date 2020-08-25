e769935c01bb34deb5777594b717f3c361689b1e
Pranav Mendiratta <pranavmendi@gmail.com>
2020-08-25 18:40:02 +0530

    Added OOPS code

--- OOPS-2/OperatorOverloading.py
+++ OOPS-2/OperatorOverloading.py
@@ -7,10 +7,11 @@ class operator:
         self.b=b
 
     def __add__(self,other): #this is the function that we have to overload if we want to overload the '+' operator
-        return self.a + other.a ,self.b + other.b
+        # this function always returns an object
+        return operator(self.a + other.a ,self.b + other.b)
 
-    def __str__(self): # this function is invoked when we call a print statement
-        return self.a,self.b
+    def __str__(self): # this function is invoked when we call a print statement and it always returns a string
+        return str(self.a)+" "+str(self.b)
 
     def __lt__(self,other): # operator overloading for '<' operator for a complex no
         return math.sqrt(self.a**2+self.b**2) < math.sqrt(other.a**2+other.b**2)
@@ -19,4 +20,4 @@ o1=operator(1,2)
 o2=operator(3,4)
 o3=o1+o2
 print(o3)
-print(o2<o3)
+print(o1<o2)
