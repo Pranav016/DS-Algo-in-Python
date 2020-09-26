65fdcdc6db71c27b8f2d0a386114847edeb27924
Pranav Mendiratta <pranavmendi@gmail.com>
2020-09-26 11:55:00 +0530

    Added LargestDataNode.py

---
+++ BinaryTrees-1.1/LargestDataNode.py
@@ -0,0 +1,49 @@
+import queue
+from sys import stdin,setrecursionlimit
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+    
+
+def levelOrderInput():
+    tlist=list(map(int, stdin.readline().strip().split(" ")))
+    start=0
+    # l=len(tlist)
+    data=tlist[start]
+    start+=1
+    if data!=-1:
+        root=Node(data)
+    else:
+        return 0
+    q=queue.Queue()
+    q.put(root)
+    while not q.empty():
+        current=q.get()
+        data=tlist[start]
+        start+=1
+        if data!=-1:
+            leftChild=Node(data)
+            current.left=leftChild
+            q.put(leftChild)
+        data=tlist[start]
+        start+=1
+        if data!=-1:
+            rightChild=Node(data)
+            current.right=rightChild
+            q.put(rightChild)
+    return root
+
+
+def largest(root):
+    if root is None:
+        return -1
+    largestLeft=largest(root.left)
+    largestRight=largest(root.right)
+    return max(root.data,largestLeft,largestRight)
+
+# main
+root=levelOrderInput()
+print(largest(root))
