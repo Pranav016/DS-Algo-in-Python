b5ee77b6829576bcba1835a5d418864fd29707d9
Pranav Mendiratta <pranavmendi@gmail.com>
2020-09-28 11:29:19 +0530

    Added Create&InsertDuplicate.py to BinaryTrees-2.2

---
+++ BinaryTrees-2.2/Create&InsertDuplicate.py
@@ -0,0 +1,65 @@
+import queue
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def insertDuplicateNode(root):
+    if root is None:
+        return
+    insertDuplicateNode(root.left)
+    insertDuplicateNode(root.right)
+    data=root.data
+    left=root.left
+    newNode=BinaryTreeNode(data)
+    root.left=newNode
+    newNode.left=left
+
+def buildLevelTree(levelorder):
+    index = 0
+    length = len(levelorder)
+    if length<=0 or levelorder[0]==-1:
+        return None
+    root = BinaryTreeNode(levelorder[index])
+    index += 1
+    q = queue.Queue()
+    q.put(root)
+    while not q.empty():
+        currentNode = q.get()
+        leftChild = levelorder[index]
+        index += 1
+        if leftChild != -1:
+            leftNode = BinaryTreeNode(leftChild)
+            currentNode.left =leftNode
+            q.put(leftNode)
+        rightChild = levelorder[index]
+        index += 1
+        if rightChild != -1:
+            rightNode = BinaryTreeNode(rightChild)
+            currentNode.right =rightNode
+            q.put(rightNode)
+    return root
+
+def printLevelATNewLine(root):
+    if root==None:
+        return
+    inputQ = queue.Queue()
+    outputQ = queue.Queue()
+    inputQ.put(root)
+    while not inputQ.empty():
+        while not inputQ.empty():
+            curr = inputQ.get()
+            print(curr.data, end=' ')
+            if curr.left!=None:
+                outputQ.put(curr.left)
+            if curr.right!=None:
+                outputQ.put(curr.right)
+        print()
+        inputQ, outputQ = outputQ, inputQ
+
+# Main
+levelOrder = [int(i) for i in input().strip().split()]
+root = buildLevelTree(levelOrder)
+insertDuplicateNode(root)
+printLevelATNewLine(root)
