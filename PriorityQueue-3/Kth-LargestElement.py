96efd332f41ee4badf2b001a4a589e1ccb272fba
Pranav Mendiratta <pranavmendi@gmail.com>
2020-10-11 16:30:06 +0530

    Updated Kth-LargestElement.py

--- PriorityQueue-3/Kth-LargestElement.py
+++ PriorityQueue-3/Kth-LargestElement.py
@@ -1,23 +1,18 @@
 import heapq
 
-# def insertHeap(heap,lst,i):
-    
-#     else:
-#         return
 
-
-def kLargest(lst, k):
-    heap=lst[:k]
-    heapq.heapify(heap)
-    for i in range(k,n,1):
-        if heap[0]<lst[i]:
-            heap[0]=lst[i]
-            heapq.heapify(heap)
-    return heap
+def kthLargest(lst, k): 
+	minHeap = lst[:k] # Add first k elements to min heap 
+	heapq.heapify(minHeap) # Add first k elements to min heap 
+	n = len(lst) 
+	for i in range(k,n): 
+		if lst[i]>minHeap[0]: 
+			heapq.heapreplace(minHeap, lst[i])
+	return minHeap[0]
 
 # Main Code
 n=int(input())
 lst=list(int(i) for i in input().strip().split(' '))
 k=int(input())
-ans=kLargest(lst, k)
-print(*ans)
+ans=kthLargest(lst, k)
+print(ans)
