1c7c90428447d0698cbd3c1c29b6ae4827c274fc
Pranav Mendiratta <pranavmendi@gmail.com>
2020-10-17 03:05:48 +0530

    Added MinStepsTo1_MemoizationDP.py and updated MinStepsTo1_Recursive.py

---
+++ DP-1/MinStepsTo1_MemoizationDP.py
@@ -0,0 +1,64 @@
+import sys
+
+# Method 1
+
+
+def minStepsA(n,dp):
+    
+    if n<=1:
+        return dp[n]
+    
+    if dp[n]!=-1:
+        return dp[n]
+
+    ans1=ans2=ans3=sys.maxsize
+
+    ans1=minStepsA(n-1,dp)
+
+    if n%2==0:
+        ans2=minStepsA(n//2,dp)
+    if n%3==0:
+        ans3=minStepsA(n//3,dp)
+    
+    ans=min(ans1,ans2,ans3)+1
+    dp[n]=ans
+    return ans
+
+# Method-2 
+
+def minStepsB(n,dp):
+    if n==1:
+        return 0
+    
+    if dp[n-1]==-1:
+        ans1=minStepsB(n-1,dp) # this will always give an ans
+        dp[n-1]=ans1
+    else:
+        ans1=dp[n-1]
+
+    ans2=sys.maxsize
+    if n%2==0:
+        if dp[n//2]==-1:
+            ans2=minStepsB(n//2,dp)
+            dp[n//2]=ans2
+        else:
+            ans2=dp[n//2]
+
+    ans3=sys.maxsize
+    if n%3==0:
+        if dp[n//3]==-1:
+            ans3=minStepsB(n//3,dp)
+            dp[n//3]=ans3
+        else:
+            ans3=dp[n//3]
+
+    ans=1+min(ans1,ans2,ans3)
+    return ans
+    
+
+# main
+sys.setrecursionlimit(1000000)
+n=int(input())
+dp=[-1 for i in range(n+1)]
+dp[0]=dp[1]=0
+print(minStepsA(n,dp))
--- DP-1/MinStepsTo1_Recursive.py
+++ DP-1/MinStepsTo1_Recursive.py
@@ -3,43 +3,30 @@
 # 2.) If n is divisible by 2, divide by 2.( if n % 2 == 0, then n = n / 2 ) ,
 # 3.) If n is divisible by 3, divide by 3. (if n % 3 == 0, then n = n / 3 ).  
 
+# this is the recursive approach to this problem statment
 
 
 import sys
 
-def minSteps(n,dp):
+def minSteps(n):
     if n==1:
         return 0
     
-    if dp[n-1]==-1:
-        ans1=minSteps(n-1,dp) # this will always give an ans
-        dp[n-1]=ans1
-    else:
-        ans1=dp[n-1]
+    ans1=ans2=ans3=sys.maxsize
 
-    ans2=sys.maxsize
+    ans1=minSteps(n-1)
+
     if n%2==0:
-        if dp[n//2]==-1:
-            ans2=minSteps(n//2,dp)
-            dp[n//2]=ans2
-        else:
-            ans2=dp[n//2]
-
-    ans3=sys.maxsize
+        ans2=minSteps(n//2)
     if n%3==0:
-        if dp[n//3]==-1:
-            ans3=minSteps(n//3,dp)
-            dp[n//3]=ans3
-        else:
-            ans3=dp[n//3]
-
-    ans=1+min(ans1,ans2,ans3)
+        ans3=minSteps(n//3)
+    
+    ans=min(ans1,ans2,ans3)+1
     return ans
-    
-    
+
 
 # main
 sys.setrecursionlimit(1000000)
 n=int(input())
 dp=[-1 for i in range(n+1)]
-print(minSteps(n,dp))
+print(minSteps(n))
