f22eb3ab570ac33c05c222cd199f123c7f9e9dc7
Pranav Mendiratta <pranavmendi@gmail.com>
2020-10-24 06:52:58 +0530

    Updated returnSubsequences.py

--- Recursion-4/returnSubsequences.py
+++ Recursion-4/returnSubsequences.py
@@ -1,19 +1,19 @@
+# Logical Intuition - 1) always do the work for the smaller string and call recursion for the bigger part
+# 2) we either consider the element or we don't
 
 def subsequences(string):
     if len(string)==0:
-        output=[]
-        output.append("")
+        output=[""]
         return output
 
     smallOutput=string[1:]
-    smallOutput=subsequences(smallOutput)
+    smallOutput=subsequences(smallOutput)# hit the base case to get the smaller problem that is a single element in the string and work upon that
     output=[]
     for ele in smallOutput:
-        output.append(ele)
+        output.append(ele) # copying elements of the returned list to the output
     for ele in smallOutput:
-        output.append(string[0]+ele)
+        output.append(string[0]+ele) # copying elements of the returned list with the element string[0]
     return output
-    
 
 
 # main
